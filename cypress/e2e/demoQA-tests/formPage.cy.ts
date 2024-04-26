5736fe0dd3ecf31ca3dfff9435e299be6e7b1c04
Sophie Onbelet <onbelet.sophie@hotmail.com>
2024-04-26 11:24:35 -0600

    Updated date of birth field

--- cypress/e2e/demoQA-tests/formPage.cy.ts
+++ cypress/e2e/demoQA-tests/formPage.cy.ts
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
-//import { PracticeFormPage } from "cypress/support/pages/formPage";
+import { PracticeFormPage } from "cypress/support/pages/formPage";
+
 Cypress.on("uncaught:exception", () => {
   return false;
 });
@@ -7,25 +8,9 @@ Cypress.on("uncaught:exception", () => {
 describe("Test different feature of uitestingplayground", () => {
   it("Test dynamicID", () => {
     cy.visit("https://demoqa.com/automation-practice-form");
-    //const formPage = new PracticeFormPage();
-    //formPage.fillForm();
-    cy.get("#firstName").type("Sophie");
-    cy.get("#lastName").type("Onbelet");
-    cy.get("#userEmail").type("sophie.onbelet@spritecloud.com");
-    cy.get("#userNumber").type("0612345678");
-    cy.get('input[name="gender"][value="Female"]').check({ force: true });
-    // cy.get('#dateOfBirthInput').clear().type("14 Dec 1990"); //theres a bug when clearing the field
-    // cy.get('#react-datepicker__month-select').click().get('value="11"').click();
-    // cy.get('#react-datepicker__year-select').click();
-    // cy.get('value="1990"').click().get('.react-datepicker__day--031').click()
-    cy.get('.subjects-auto-complete__value-container').click().type("c")
-    cy.get('div[id="react-select-2-option-0"]').click();
-    cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(3) > .custom-control-label').click();
-    cy.get('#currentAddress').type("Tonala 201");
-    cy.get('#state > div > div.css-1wy0on6 > div').click();
-    cy.get('#react-select-3-option-3').click();
-    cy.get('#city > .css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').click();
-    cy.get('#react-select-4-option-0').click();
-    cy.get('#submit').click();
+    const formPage = new PracticeFormPage();
+    formPage.fillForm();
+    formPage.addGender("Female");
+    formPage.clickSubmitButton();
   });
 });
--- cypress/support/pages/formPage.ts
+++ cypress/support/pages/formPage.ts
@@ -1,28 +1,56 @@
 export class PracticeFormPage {
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  mobile: string;
+  dateOfBirth: string;
+  subjects: string;
+  hobbies: string;
+  address: string;
+  state: string;
+  city: string;
+  submit: string;
 
-    firstName: string;
-    lastName: string;
-    emailAddress: string;
-    gender: string;
-    mobile: string;
-    
-    constructor() {
-        this.firstName = "#firstName";
-        this.lastName = "#lastName";
-        this.emailAddress = "#userEmail";
-        this.gender = 'input[name="gender"][value="replaceMe"]';
-       this.mobile = "#userNumber";
-    }
+  constructor() {
+    this.firstName = "#firstName";
+    this.lastName = "#lastName";
+    this.emailAddress = "#userEmail";
+    this.mobile = "#userNumber";
+    this.dateOfBirth = "#dateOfBirthInput";
+    this.subjects = ".subjects-auto-complete__value-container";
+    this.hobbies =
+      "#hobbiesWrapper > .col-md-9 > :nth-child(3) > .custom-control-label";
+    this.address = "#currentAddress";
+    this.state = "#state > div > div.css-1wy0on6 > div";
+    this.city =
+      "#city > .css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer";
+    this.submit = "#submit";
+  }
 
-    fillForm() {
-            cy.get(this.firstName).type("Sophie");
-            cy.get(this.lastName).type("Onbelet");
-            cy.get(this.emailAddress).type("sophie.onbelet@spritecloud.com");
-        //     this.selectGender(user.gender);
-            cy.get(this.mobile).type("0600000000");
-        //     cy.get(this.currentaddress).type(user.currentAddress);
-        //     this.fillSubjects(user.subjects);
-        //     this.selectHobby(user.hobbies);
-          }
+  fillForm() {
+    cy.get(this.firstName).type("Sophie");
+    cy.get(this.lastName).type("Onbelet");
+    cy.get(this.emailAddress).type("sophie.onbelet@spritecloud.com");
+    cy.get(this.mobile).type("0612345678");
+    cy.get(this.dateOfBirth)
+      .click()
+      .type("{selectall}")
+      .type("14 Dec 1990{enter}"); //theres a bug when clearing the field, so I used selectAll and enter instead.
+    cy.get(this.subjects).click().type("c");
+    cy.get('div[id="react-select-2-option-0"]').click();
+    cy.get(this.hobbies).click();
+    cy.get(this.address).type("Tonala 150");
+    cy.get(this.state).click();
+    cy.get("#react-select-3-option-3").click();
+    cy.get(this.city).click();
+    cy.get("#react-select-4-option-0").click();
+  }
 
+  addGender(gender: string) {
+    cy.get(`[value="${gender}"]`).check({ force: true });
+  }
+
+  clickSubmitButton() {
+    cy.get(this.submit).click();
+  }
 }
